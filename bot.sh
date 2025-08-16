4b72556d33b6069be91420a199868b7f8bc51a30
nyaruku <38096703+nyaruku@users.noreply.github.com>
2025-08-16 19:36:28 +0000

    bot.sh (service script) added

---
+++ bot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Checking for existing bot instances..."
+pkill -f "nekoha.moe-bot"
+
+while true; do
+    echo "Starting Bot..."
+    ./out/build/x86-debug-linux/nekoha.moe-bot
+    echo "Bot crashed. Restarting..."
+    sleep 1
+done
